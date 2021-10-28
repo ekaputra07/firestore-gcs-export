e0cd9cf094de96d976b736adf5ecda76ec1d594c
Eka Putra <ekaputra07@gmail.com>
2021-10-29 08:15:32 +1100

    feat: accept args via argparse

--- export.py
+++ export.py
@@ -2,32 +2,18 @@ import os
 import sys
 import json
 import jsonlines
+import argparse
 from tempfile import NamedTemporaryFile
 from datetime import datetime
 from google.cloud import firestore, storage
 from google.api_core.datetime_helpers import DatetimeWithNanoseconds
-from dotenv import load_dotenv
-
-load_dotenv()
 
 IMPORT_TS = datetime(1970, 1, 1)
 IMPORT_OPERATION = 'IMPORT'
 IMPORT_EVENT_ID = ''
 
-PROJECT_ID = os.getenv('PROJECT_ID')
-COLLECTION = os.getenv('COLLECTION')
-GCS_BUCKET = os.getenv('GCS_BUCKET')
-BATCH_SIZE = int(os.getenv('BATCH_SIZE', default=500))
 
-if not PROJECT_ID or not COLLECTION or not GCS_BUCKET:
-    print('[ERROR] missing PROJECT_ID, COLLECTION or GCS_BUCKET!')
-    sys.exit(1)
-
-db = firestore.Client(project=PROJECT_ID)
-gcs = storage.Client(project=PROJECT_ID)
-
-
-def to_items(result):
+def to_items(project, result):
     items = []
     first_doc = None
     last_doc = None
@@ -50,7 +36,7 @@ def to_items(result):
         last_doc = result[-1]
         for doc in result:
             doc_path = doc.reference.path
-            doc_name = f'projects/{PROJECT_ID}/databases/(default)/documents/{doc_path}'
+            doc_name = f'projects/{project}/databases/(default)/documents/{doc_path}'
             item = {
                 'timestamp': IMPORT_TS.isoformat(),
                 'event_id': IMPORT_EVENT_ID,
@@ -63,7 +49,7 @@ def to_items(result):
     return items, first_doc, last_doc
 
 
-def upload_items_to_gcs(items, bucket_name, object_path):
+def upload_items_to_gcs(client, items, bucket_name, object_path):
     if items:
         # create temp file to store the data
         f = NamedTemporaryFile(delete=False)
@@ -74,7 +60,7 @@ def upload_items_to_gcs(items, bucket_name, object_path):
             writer.write_all(items)
 
         # upload to GCS
-        bucket = gcs.get_bucket(bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(object_path)
         blob.upload_from_filename(filename=filename)
 
@@ -84,17 +70,17 @@ def upload_items_to_gcs(items, bucket_name, object_path):
             f'[OK] {len(items)} rows uploaded to gs://{bucket_name}/{object_path}')
 
 
-def query(collection, batch_size, last_doc=None):
+def query(client, collection, batch_size, last_doc=None):
     if last_doc:
-        result = db.collection(collection) \
+        result = client.collection(collection) \
             .start_after(last_doc) \
             .limit(batch_size) \
             .get()
     else:
-        result = db.collection(collection) \
+        result = client.collection(collection) \
             .limit(batch_size) \
             .get()
-    return to_items(result)
+    return to_items(project=client.project, result=result)
 
 
 def create_gcs_object_path(collection, first_doc, last_doc):
@@ -103,20 +89,58 @@ def create_gcs_object_path(collection, first_doc, last_doc):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Export Firestore collection to GCS')
+    parser.add_argument(
+        '--project', help='Firebase Project ID for project containing the Cloud Firestore database.', required=True)
+    parser.add_argument('--source-collection',
+                        help='The path of the the Cloud Firestore Collection to read from.', required=True)
+    parser.add_argument(
+        '--dest-bucket', help='The GCS bucket to store the export files.', required=True)
+    parser.add_argument(
+        '--batch-size', help='The GCS bucket to store the export files.', default=500, type=int)
+    parser.add_argument(
+        '--start-after', help='Firestore document ID to use as a start_after cursor for the query.')
+    args = parser.parse_args()
+
+    project = args.project
+    collection = args.source_collection
+    bucket = args.dest_bucket
+    batch_size = args.batch_size
+    start_after = args.start_after
+
+    db = firestore.Client(project=project)
+    gcs = storage.Client(project=project)
+
     last_doc = None
+    if start_after:
+        last_doc = db.collection(collection).document(start_after).get()
+        if not last_doc.exists:
+            print(f'[ERR] Document with id={start_after} does not exists.')
+            sys.exit(1)
 
     # first call
-    items, first, last = query(COLLECTION, BATCH_SIZE, last_doc=last_doc)
+    items, first, last = query(
+        client=db,
+        collection=collection,
+        batch_size=batch_size,
+        last_doc=last_doc)
     last_doc = last if last else None
     if last_doc:
-        upload_items_to_gcs(items,
-                            GCS_BUCKET,
-                            create_gcs_object_path(COLLECTION, first, last))
+        upload_items_to_gcs(client=gcs,
+                            items=items,
+                            bucket_name=bucket,
+                            object_path=create_gcs_object_path(collection, first, last))
 
     while last_doc is not None:
-        items, first, last = query(COLLECTION, BATCH_SIZE, last_doc=last_doc)
+        items, first, last = query(
+            client=db,
+            collection=collection,
+            batch_size=batch_size,
+            last_doc=last_doc)
         last_doc = last if last else None
         if last_doc:
-            upload_items_to_gcs(items,
-                                GCS_BUCKET,
-                                create_gcs_object_path(COLLECTION, first, last))
+            upload_items_to_gcs(client=gcs,
+                                items=items,
+                                bucket_name=bucket,
+                                object_path=create_gcs_object_path(collection, first, last))
